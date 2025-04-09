e2360cc329dfad7cdaca42e817a174f6f63b0cf9
sunjom <saysaysay223@naver.com>
2025-04-09 11:55:05 +0900

    유효성 검사 까지 완료

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 const CampGround = require('./models/campground');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate')
+const catchAsync = require('./utils/catchAsync')
+const ExpressError =require('./utils/ExpressError');
+const Joi = require('joi');
+const { campgroundSchema } = require('./schemas');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp').then((res)=>{
     console.log("Connected")
@@ -16,6 +20,17 @@ app.set('views',path.join(__dirname,'views'));
 //form 데이터를 res.body에 담아오기 위해 필요
 app.use(express.urlencoded({extends:true}))
 app.use(methodOverride('_method'))
+
+const validateCampground = (req,res,next) => {
+    const {error} = campgroundSchema.validate(req.body)
+    if(error){
+        const msg = error.details.map(el => el.message).join(",")
+        throw new ExpressError(msg,400); 
+    }else{
+        next()
+    }
+}
+
 app.get('/',(req,res)=>{ 
     res.render('home');
 })
@@ -28,33 +43,43 @@ app.get('/campgrounds/new',async (req,res)=>{
     res.render('campgrounds/make')
 })
 
-app.get('/campgrounds/:id',async(req,res) =>{
+app.get('/campgrounds/:id',catchAsync(async(req,res) =>{
     const campground = await CampGround.findById(req.params.id);
     res.render('campgrounds/show',{campground})
-})
+}))
 
-app.get('/campgrounds/:id/update',async(req,res) =>{
+app.get('/campgrounds/:id/update',catchAsync(async(req,res) =>{
     const campground = await CampGround.findById(req.params.id);
     res.render('campgrounds/update',{campground})
-})
+}))
 
-app.delete('/campgrounds/:id', async(req,res)=>{
+app.delete('/campgrounds/:id', catchAsync(async(req,res,next)=>{
     await CampGround.findByIdAndDelete(req.params.id)
     res.redirect('/campgrounds');
-})
+}))
 
-app.post('/campgrounds',async(req,res) =>{
+app.post('/campgrounds',validateCampground,catchAsync(async(req,res,next) =>{
+    //if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
     const data = req.body.campground
     const campground = new CampGround(data)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.put('/campgrounds/:id', async(req,res) =>{
+app.put('/campgrounds/:id',validateCampground, catchAsync(async(req,res) =>{
     const data = await CampGround.findByIdAndUpdate(req.params.id,req.body.campground);
     res.redirect(`/campgrounds/${req.params.id}`)
+}))
+
+app.all('*',(req,res,next) => {
+    next(new ExpressError('Page Not Found',404));
 })
 
+app.use((err,req,res,next) => {
+    const {status=500} = err;
+    if(!err.message) err.message = 'Oh No, Something Went Wrong'
+    res.status(status).render('error',{err})
+})
 
 
 app.listen(3000, () => {
