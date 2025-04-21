cc33dad3a773219703bc39b6b6c8bd565bcbd534
sunjom <saysaysay223@naver.com>
2025-04-21 18:06:55 +0900

    test

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 app.use((req,res,next)=>{
     res.locals.currentUser = req.user
+    console.log(res.locals.currentUser);
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
