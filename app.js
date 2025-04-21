c3d94a63edd3ed99a0f208326a709db14d8d5432
sunjom <saysaysay223@naver.com>
2025-04-21 23:57:06 +0900

    test13

--- app.js
+++ app.js
@@ -138,5 +138,6 @@ app.use((err,req,res,next) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('Serving on port 3000')
+    console.log(port);
+    console.log(process.env.DB_URL)
 })
